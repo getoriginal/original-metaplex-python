0c4f1a34e02c0f90c062a23d3cdf838ae2538a96
Don Demetrius <don@oasisgg.com>
2024-03-15 14:43:53 +0000

    Tests for token_owned_escrow

--- src/token_metadata/generated/accounts/token_owned_escrow.py
+++ src/token_metadata/generated/accounts/token_owned_escrow.py
@@ -3,8 +3,10 @@ from dataclasses import dataclass
 
 import borsh_construct as borsh
 from anchorpy.borsh_extension import BorshPubkey
-from anchorpy.coder.accounts import ACCOUNT_DISCRIMINATOR_SIZE
-from anchorpy.error import AccountInvalidDiscriminator
+
+# TODO_ORIGINAL
+# from anchorpy.coder.accounts import ACCOUNT_DISCRIMINATOR_SIZE
+# from anchorpy.error import AccountInvalidDiscriminator
 from anchorpy.utils.rpc import get_multiple_accounts
 from solana.rpc.async_api import AsyncClient
 from solana.rpc.commitment import Commitment
@@ -73,11 +75,13 @@ class TokenOwnedEscrow:
 
     @classmethod
     def decode(cls, data: bytes) -> "TokenOwnedEscrow":
-        if data[:ACCOUNT_DISCRIMINATOR_SIZE] != cls.discriminator:
-            raise AccountInvalidDiscriminator(
-                "The discriminator for this account is invalid"
-            )
-        dec = TokenOwnedEscrow.layout.parse(data[ACCOUNT_DISCRIMINATOR_SIZE:])
+        # TODO_ORIGINAL
+        # if data[:ACCOUNT_DISCRIMINATOR_SIZE] != cls.discriminator:
+        #     raise AccountInvalidDiscriminator(
+        #         "The discriminator for this account is invalid"
+        #     )
+        # dec = TokenOwnedEscrow.layout.parse(data[ACCOUNT_DISCRIMINATOR_SIZE:])
+        dec = TokenOwnedEscrow.layout.parse(data)
         return cls(
             key=types.key.from_decoded(dec.key),
             base_token=dec.base_token,
